61d6261d411af30a2aa74042cbd78284ee01ee60
sambit24795 <50554112+sambit24795@users.noreply.github.com>
2021-05-22 19:42:34 +0530

    client done

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useRef, useCallback, useState } from "react";
 
 import classes from "./Board.module.css";
 import Modal from "../ui/Modal";
-import Buttons from "../utils/Buttons";
+import Buttons from "./Buttons";
+import socket from "../utils/connectSocket";
 
 interface IBoardProps {}
 
@@ -12,9 +13,12 @@ const Board: React.FunctionComponent<IBoardProps> = (props) => {
   const [mouseY, setMouseY] = useState<number | null>(null);
   const [color, setColor] = useState<string>("#ff0000");
   const [size, setSize] = useState<number>(20);
-  const [dataUrl, setDataUrl] = useState<string | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(true);
+  const [username, setUsername] = useState<string>("");
+  const [drawing, setDrawing] = useState<boolean>(false);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const drawCircle = (
     ctx: CanvasRenderingContext2D,
@@ -107,27 +111,104 @@ const Board: React.FunctionComponent<IBoardProps> = (props) => {
     []
   );
 
-  useEffect(() => {
-    const timer: NodeJS.Timeout = setTimeout(() => {
-      const base64Image = canvasRef.current?.toDataURL("image/png");
-      console.log(base64Image);
-      if (base64Image) {
-        setDataUrl(base64Image);
+  const submitHandler = async () => {
+    if (!inputRef.current?.value) {
+      return;
+    }
+
+    const res = await fetch(
+      `http://localhost:5000/user/${inputRef.current.value}`,
+      {
+        method: "POST",
       }
-    }, 800);
+    );
+    if (res.status === 409) {
+      alert("choose different name");
+      return;
+    }
+    const data = await res.json();
+    if (data.user.username) {
+      setShowModal(false);
+    }
+    setUsername(data.user.username);
+    setColor(data.user.color);
+  };
+
+  const saveHandler = () => {
+    const ctx = canvasRef.current!;
+    if (ctx && username) {
+      ctx.toBlob(async (blob) => {
+        const random = Math.random().toString(36).substring(7);
+        const file = new File([blob!], `canvas-image-${random}.png`, {
+          type: "image/png",
+        });
+
+        const formData = new FormData();
+        formData.append("image", file);
+
+        const res = await fetch("http://localhost:5000/image/upload", {
+          method: "POST",
+          body: formData,
+        });
+
+        const data = await res.json();
+        console.log(data);
+      });
+    }
+  };
+
+  useEffect(() => {
+    socket.on("node-canvas-data", (data) => {
+      console.log("canvas data", data);
+      const interval = setInterval(() => {
+        if (drawing) {
+          return;
+        }
+        setDrawing(true);
+        clearInterval(interval);
+        const image = new Image();
+        const canvas = canvasRef.current;
+        const ctx = canvas?.getContext("2d");
+        image.onload = () => {
+          ctx?.drawImage(image, 0, 0);
+          setDrawing(false);
+        };
+        image.src = data;
+      });
+    });
+
+    const timer: NodeJS.Timeout = setTimeout(() => {
+      const ctx = canvasRef.current!;
+      const base64Image = ctx.toDataURL("image/png");
+      const file = ctx.toBlob((blob) => {
+        const blb = new File([blob!], "canvas-image.png", {
+          type: "image/png",
+        });
+        console.log(blb);
+        return blb;
+      });
+
+      console.log("file", file);
+      console.log("executing");
+      if (base64Image) {
+        socket.emit("canvas-data", base64Image);
+      }
+    }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [canvasRef, mouseX, mouseY]);
+  }, [drawing, mouseX, mouseY]);
 
   return (
     <React.Fragment>
-      <Modal>
-        <label>Enter your name:</label>
-        <input type="text" />
-        <button>Submit</button>
-      </Modal>
+      {showModal && (
+        <Modal>
+          <label>Enter your name:</label>
+          <input type="text" ref={inputRef} />
+          <button onClick={() => submitHandler()}>Submit</button>
+        </Modal>
+      )}
       <div className={classes.container}>
         <Buttons
           setColor={setColor}
@@ -137,7 +218,7 @@ const Board: React.FunctionComponent<IBoardProps> = (props) => {
         />
         <canvas
           className={classes["canvas-element"]}
-          height="600"
+          height="500"
           width="800"
           id="canvas"
           ref={canvasRef}
@@ -145,6 +226,12 @@ const Board: React.FunctionComponent<IBoardProps> = (props) => {
           onMouseUp={(event) => mouseUpHandler(event)}
           onMouseMove={(event) => mouseMoveHandler(event)}
         ></canvas>
+        <button
+          className={classes["save-button"]}
+          onClick={() => saveHandler()}
+        >
+          Save
+        </button>
       </div>
     </React.Fragment>
   );
